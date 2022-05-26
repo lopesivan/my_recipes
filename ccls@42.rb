56970cf364eef752c1dd129cc3ee04f285537926
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-05-26 15:54:26 -0300

    Renomeia o arquivo ccls.rb ➜ ccls@42.rb

--- ccls@42.rb
+++ ccls@42.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Ccls < Formula
+class CclsAT42< Formula
   desc "C/C++/ObjC language server"
   homepage "https://github.com/MaskRay/ccls"
   url "https://github.com/MaskRay/ccls.git"
