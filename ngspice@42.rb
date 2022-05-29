ac9251164f240ef2037672cf5e2dd6b9f03ee559
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-05-29 05:47:52 -0300

    Adiciona o arquivo ngspice@42.rb

---
+++ ngspice@42.rb
@@ -0,0 +1,20 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class NgspiceAT42< Formula
+  desc "Spice circuit simulator"
+  homepage "https://ngspice.sourceforge.io/"
+  url "https://github.com/lopesivan/build-neovim.git"
+  version "37.0"
+  license "Apache-2.0"
+  revision 4
+  head "https://github.com/lopesivan/build-ngspice.git", branch: "master"
+
+  def install
+      system "/usr/local/bin/bash", "build-ngspice.sh", "#{prefix}"
+  end
+
+  test do
+    system "false"
+  end
+end
