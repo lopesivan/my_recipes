fec390c04d5b4891350944dfaed97fc0699bd613
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-05-29 10:09:27 -0300

    using cmake without bash

--- neovim@42.rb
+++ neovim@42.rb
@@ -4,14 +4,37 @@
 class NeovimAT42< Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
   homepage "https://neovim.io/"
-  url "https://github.com/lopesivan/build-neovim.git"
+  url "https://github.com/neovim/neovim.git"
   version "0.8.0"
   license "Apache-2.0"
   revision 4
-  head "https://github.com/lopesivan/build-neovim.git", branch: "master"
+  head "https://github.com/neovim/neovim.git", branch: "master"
 
   def install
-      system "/usr/local/bin/bash", "build-neovim.sh", "#{prefix}"
+    args_third_party = %W[
+      --no-warn-unused-cli
+      -DCMAKE_C_COMPILER:FILEPATH=/usr/local/bin/clang
+      -DCMAKE_CXX_COMPILER:FILEPATH=/usr/local/bin/clang++
+    ]
+
+    args = %W[
+      --no-warn-unused-cli
+      -DCMAKE_BUILD_TYPE=Release
+      -DCMAKE_C_COMPILER:FILEPATH=/usr/local/bin/clang
+      -DCMAKE_CXX_COMPILER:FILEPATH=/usr/local/bin/clang++
+      -DCMAKE_INSTALL_PREFIX=#{prefix}
+    ]
+
+    mkdir ".deps" do
+      system "/usr/local/bin/cmake", *args_third_party, "../third-party"
+      system "make"
+    end
+
+    mkdir "build" do
+      system "/usr/local/bin/cmake", *args, ".."
+      system "make"
+    end
+
   end
 
   test do
