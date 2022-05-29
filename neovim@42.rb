0dbcf451625863f32d56ecc95c97048124a70f83
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-05-29 05:06:31 -0300

    Atualiza o arquivo neovim@42.rb

--- neovim@42.rb
+++ neovim@42.rb
@@ -4,29 +4,14 @@
 class NeovimAT42< Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
   homepage "https://neovim.io/"
-  url "https://github.com/neovim/neovim.git"
+  url "https://github.com/lopesivan/build-neovim.git"
   version "0.8.0"
   license "Apache-2.0"
   revision 4
-  head "https://github.com/neovim/neovim.git", branch: "master"
+  head "https://github.com/lopesivan/build-neovim.git", branch: "master"
 
   def install
-    args = %W[
-      -DCMAKE_INSTALL_PREFIX=#{prefix}
-      -DCMAKE_BUILD_TYPE=Release"
-    ]
-
-    mkdir ".deps" do
-      system "cmake", "../third-party"
-      system "make"
-    end
-
-    mkdir "build" do
-      system "cmake", *args, ".."
-      system "make"
-      system "make", "install"
-    end
-
+      system "/usr/local/bin/bash", "build-neovim.sh"
   end
 
   test do
