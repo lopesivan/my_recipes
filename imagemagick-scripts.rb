5c328dee0f47d880a4da57e79a6f29173099c912
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-05-29 13:51:43 -0300

    Atualiza o arquivo imagemagick-scripts.rb

--- imagemagick-scripts.rb
+++ imagemagick-scripts.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class GitScripts < Formula
+class ImagemagickScripts < Formula
   desc "Using git scripts like programs"
   homepage "https://github.com/lopesivan/imagemagick-scripts.git"
   version "1.1.4"
