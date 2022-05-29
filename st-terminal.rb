5d90545e0510bb53908250e6226f0b5664fd2bc3
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-05-29 08:31:45 -0300

    usando makefile

--- st-terminal.rb
+++ st-terminal.rb
@@ -12,42 +12,8 @@ class StTerminal < Formula
   license "GPL-3.0-or-later"
 
   def install
-    cflags = %W[
-      -I/usr/X11R6/include
-      -I/usr/local/include
-      -I/usr/include/freetype2
-      -I/usr/include/libpng16
-      -I/usr/include/freetype2
-      -I/usr/include/libpng16
-      -I/usr/include/harfbuzz
-      -I/usr/include/glib-2.0
-      -I/usr/lib/x86_64-linux-gnu/glib-2.0/include
-      -DVERSION=\"0.8.4\"
-      -D_XOPEN_SOURCE=600
-    ]
-
-    libs = %W[
-    -L/usr/X11R6/lib
-    -lm
-    -lrt
-    -lX11
-    -lutil
-    -lXft
-    -lXrender
-    -lfontconfig
-    -lfreetype
-    -lfreetype
-    -lharfbuzz
-    ]
-
-    system "c99", *cflags, "-O", "-c", "st.c"
-    system "c99", *cflags, "-O", "-c", "x.c"
-    system "c99", *cflags, "-O", "-c", "boxdraw.c"
-    system "c99", *cflags, "-O", "-c", "hb.c"
-    system "c99", "-o", "st", "st.o", "x.o", "boxdraw.o", "hb.o", *libs
-
-    system "mkdir", "-p", "#{prefix}/bin"
-    system "cp", "-f", "st", "#{prefix}/bin"
+    system "make"
+    system "make", "PREFIX=#{prefix}", "install"
   end
 
   test do
