11c5da961d02fb07deda18087ebc94490b31fa6f
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-02 11:38:53 -0300

    Atualiza o arquivo bangsh.rb

--- bangsh.rb
+++ bangsh.rb
@@ -2,33 +2,21 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Bangsh < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/lopesivan/bangsh"
-  version ""
-  sha256 ""
-  license ""
-
-  # depends_on "cmake" => :build
+  desc "Bang.sh - for easy Shell Scripting"
+  homepage "https://github.com/bangsh/bangsh"
+  version "1.1.0"
+  url "https://github.com/bangsh/bangsh.git",
+    branch: "master",
+    revision: "2c9c70d73fd161c57ffd3595d95e6285698a9c59"
+  license "MIT"
+  revision 1
+  head "https://github.com/bangsh/bangsh.git", branch: "master"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "make","prefix=#{prefix}", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test bangsh`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
