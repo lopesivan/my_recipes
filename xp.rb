aeb542e6f8f26c3b81b5e7d2eb78eaca3bde2ebb
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-02 11:04:32 -0300

    Atualiza o arquivo neroaac.rb

--- xp.rb
+++ xp.rb
@@ -1,18 +1,20 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Neroaac < Formula
-  desc "Nero AAC Encoder/Decoder/Tag"
+class Xp < Formula
+  desc ""
   homepage ""
-  url "http://192.168.2.43:8899/my_gnu/neroAac-1.5.4.0.tar.gz"
-  version "1.5.4.0"
-  sha256 "8cecafdb27edad16b7018352001378947f4271413c634b909257d80a350a9c60"
-  license "gpl-3.0"
+  license ""
+  head "https://github.com/lopesivan/xp.git"
+
+  # depends_on "cmake" => :build
 
   def install
-    bin.install "neroAacDec" => "neroaacdec"
-    bin.install "neroAacEnc" => "neroaacenc"
-    bin.install "neroAacTag" => "neroaactag"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
   test do
@@ -20,7 +22,7 @@ class Neroaac < Formula
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test neroAac`. Options passed
+    # software. Run the test with `brew test xp`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
