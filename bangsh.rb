894e6ceed906b155317f41e733c9e1bc7b75e489
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-02 14:54:31 -0300

    Atualiza o arquivo bangsh.rb

--- bangsh.rb
+++ bangsh.rb
@@ -4,15 +4,18 @@
 class Bangsh < Formula
   desc "Bang.sh - for easy Shell Scripting"
   homepage "https://github.com/bangsh/bangsh"
-  version "1.1.0"
+  version "0.1.1"
   url "https://github.com/bangsh/bangsh.git",
-    branch: "master",
+    branch: "main",
     revision: "2c9c70d73fd161c57ffd3595d95e6285698a9c59"
   license "MIT"
   revision 1
   head "https://github.com/bangsh/bangsh.git", branch: "master"
 
   def install
+    system "wget", "-O", "bangsh-makefile-20220602-86e51ea.diff",
+      "https://raw.githubusercontent.com/lopesivan/bangsh/patches/bangsh-makefile-20220602-86e51ea.diff"
+    system "patch", "-Np1", "-i", "bangsh-makefile-20220602-86e51ea.diff"
     system "make","prefix=#{prefix}", "install"
   end
 
