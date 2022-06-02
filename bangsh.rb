dd55db0b0358cfa866efc6b9d04dc1434a19443d
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-02 15:08:37 -0300

    update

--- bangsh.rb
+++ bangsh.rb
@@ -12,14 +12,21 @@ class Bangsh < Formula
   revision 1
   head "https://github.com/bangsh/bangsh.git", branch: "master"
 
+  # # https://zlib.net/zlib_how.html
+  # resource "test_artifact" do
+  #   url "https://zlib.net/zpipe.c"
+  #   version "20051211"
+  #   sha256 "68140a82582ede938159630bca0fb13a93b4bf1cb2e85b08943c26242cf8f3a6"
+  # end
+
   patch do
     url "https://raw.githubusercontent.com/lopesivan/bangsh/patches/bangsh-makefile-20220602-86e51ea.diff"
     sha256 "1ae3f11e90ada2638d3ab61605a004354c14167a86917d515f1961246b1ebba0"
   end
 
-  def install
-    system "make","prefix=#{prefix}", "install"
-  end
+  # def install
+  #   system "make","prefix=#{prefix}", "install"
+  # end
 
   test do
     system "false"
