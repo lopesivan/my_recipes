25ec9a84ca59b1c2fb9d3017f50fc05da5b2b774
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-02 16:23:06 -0300

    Atualiza o arquivo xp.rb

--- xp.rb
+++ xp.rb
@@ -16,28 +16,10 @@ class Xp < Formula
   depends_on "bangsh" => :build
 
   def install
-    system "make","PREFIX=#{prefix}", "install"
-  end
-
-
-  def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    #system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "make","prefix=#{prefix}", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test xp`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
