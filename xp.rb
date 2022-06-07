46ca604424d9fe108a1a122ef6e7336e1efeab42
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-07 17:23:33 -0300

    Atualiza o arquivo xp.rb

--- xp.rb
+++ xp.rb
@@ -14,9 +14,25 @@ class Xp < Formula
   license "GPL-3.0-or-later"
 
   depends_on "bangsh" => :build
+  depends_on "bash-redis" => :build
 
   def install
-	#system "cp", "-r", "etc", "exec", "modules", "tasks", "tests", "xp", #{prefix}
+    inreplace "tasks/add.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/cat.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/copy.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/c.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/e.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/i.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/keys.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/link.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/ls.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/ls.sh.OLD", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/pop.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/push.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/rm.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/s.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+    inreplace "tasks/x.sh", "__REDIS_BASH_LIB__", "${HOME}/.linuxbrew/Cellar/redis-bash/1.0.0/redis-bash-lib"
+
     system "make","prefix=#{prefix}", "install"
   end
 
