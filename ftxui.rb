d48b3d01a3b89259e5ecdafc0413c0681c2377f1
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-06-08 06:15:33 -0300

    ftxui: Functional Terminal (X) User interface

---
+++ ftxui.rb
@@ -0,0 +1,30 @@
+class Ftxui < Formula
+  desc "FTXUI - Functional Terminal (X) User interface"
+  homepage "https://github.com/ArthurSonzogni/FTXUI"
+  url "https://github.com/ArthurSonzogni/FTXUI.git"
+  version "3.0.0"
+  head "https://github.com/ArthurSonzogni/FTXUI.git",
+    branch: "master"
+  license "GPL-3.0-or-later"
+
+  def install
+    args = %W[
+      --no-warn-unused-cli
+      -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE
+      -DCMAKE_BUILD_TYPE:STRING=Debug
+      -DCMAKE_C_COMPILER:FILEPATH=/usr/local/bin/clang
+      -DCMAKE_CXX_COMPILER:FILEPATH=/usr/local/bin/clang++
+      -DCMAKE_INSTALL_PREFIX=#{prefix}
+    ]
+
+    mkdir "build" do
+      system "/usr/local/bin/cmake", *args, "..", "-G", "Ninja"
+      system "/usr/bin/ninja"
+      system "/usr/bin/ninja", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
