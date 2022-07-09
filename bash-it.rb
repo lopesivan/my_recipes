be339574855d56c4937f6213f1d92f4477121c55
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-09 19:08:56 -0300

    Atualiza o arquivo bash-it.rb

--- bash-it.rb
+++ bash-it.rb
@@ -13,6 +13,9 @@ class BashIt < Formula
   patch do
     url "https://raw.githubusercontent.com/lopesivan/my_patches/main/bash-it/bash-it-my_local_changes-20220709-9a500059.diff"
     sha256 "a7ef77531453232c4288b1bf81e00845d953efae88642ac7b2c36398f9f9a52b"
+
+    url "https://raw.githubusercontent.com/lopesivan/my_patches/main/bash-it/bash-it-custom-20220709-6094da84.diff"
+    sha256 "f0044e0c538caf562d06072dde1a13dbaa44c3e0a9c35c095b9acb154d6e1fc3"
   end
 
   def install
