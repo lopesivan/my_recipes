ef3460c4da58b00f16c17404fb264277d2ec8f92
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-09 18:42:37 -0300

    Atualiza o arquivo bash-it.rb

--- bash-it.rb
+++ bash-it.rb
@@ -4,10 +4,16 @@
 class BashIt < Formula
   desc "Bash-it is a collection of community Bash commands and scripts for Bash 3.2+. (And a shameless ripoff of oh-my-zsh smiley."
   homepage "https://github.com/Bash-it/bash-it"
-  url "https://github.com/Bash-it/bash-it", using: :git
-  version "0.4.2"
+  url "https://github.com/Bash-it/bash-it.git",
+    revision: "2ef5d483e38d790cb1c32ea8999dfbc244ff2f91"
+  version "3.8.2"
   head "https://github.com/Bash-it/bash-it", branch: "master", using: :git
   license "GPL-3.0-or-later"
+
+  patch do
+    url "https://raw.githubusercontent.com/lopesivan/my_patches/main/bash-it/bash-it-my_local_changes-20220709-9a500059.diff"
+    sha256 "a7ef77531453232c4288b1bf81e00845d953efae88642ac7b2c36398f9f9a52b"
+  end
 
   def install
     prefix.install Dir["*"]
