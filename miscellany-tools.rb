3630574de095af7ae2f03107c29f19a4d7182a1d
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-07-14 02:17:29 -0300

    Atualiza o arquivo miscellany-tools.rb

--- miscellany-tools.rb
+++ miscellany-tools.rb
@@ -2,19 +2,19 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class MiscellanyTools < Formula
-  desc "Commnad line miscellany tools."
-  homepage "https://github.com/lopesivan/miscellany"
-  url "https://github.com/lopesivan/miscellany.git",
-    branch: "main",
-    using: :git
-  version "1.2"
-  head "https://github.com/lopesivan/miscellany.git",
-    branch: "master",
-    using: :git
-  license "GPL-3.0-or-later"
+  desc 'Commnad line miscellany tools.'
+  homepage 'https://github.com/lopesivan/miscellany'
+  url 'https://github.com/lopesivan/miscellany.git',
+      branch: 'main',
+      using: :git
+  version '1.2'
+  head 'https://github.com/lopesivan/miscellany.git',
+       branch: 'master',
+       using: :git
+  license 'GPL-3.0-or-later'
 
   def install
-    prefix.install "zeta-note", "tmux2"
+    prefix.install 'zeta-note', 'tmux2'
 
     # bin.mkdir
 
@@ -22,22 +22,22 @@ class MiscellanyTools < Formula
     #   bin.install_symlink "#{prefix}/binary/#{cmd}"
     # end
 
-    bin.install "bin/bakefile-0.2.12.sh" => "bakefile.sh"
-    bin.install "binary/glow" => "glow"
-    bin.install "binary/exa" => "exa"
-    bin.install "bin/lsp.sh" => "lsp"
-    bin.install "bin/lsperm.sh" => "lsperm"
-    bin.install "bin/fennel-1.1.0" => "fennel"
+    bin.install 'bin/bakefile-0.2.12.sh' => 'bakefile.sh'
+    # bin.install "binary/glow" => "glow"
+    bin.install 'binary/exa' => 'exa'
+    bin.install 'bin/lsp.sh' => 'lsp'
+    bin.install 'bin/lsperm.sh' => 'lsperm'
+    bin.install 'bin/fennel-1.1.0' => 'fennel'
     # bin.install "bin/tree-sitter-0.20.2" => "tree-sitter"
-    bin.install "bin/java-lsp.sh" => "java-lsp.sh"
-    bin.install "bin/winetricks" => "winetricks"
-    bin.install "bin/cpp-ctags.sh" => "cpp-ctags"
-    bin.install "bin/cheat-client.sh" => "cht.sh"
+    bin.install 'bin/java-lsp.sh' => 'java-lsp.sh'
+    bin.install 'bin/winetricks' => 'winetricks'
+    bin.install 'bin/cpp-ctags.sh' => 'cpp-ctags'
+    bin.install 'bin/cheat-client.sh' => 'cht.sh'
     # bin.install "bin/cheat-client.sh" => "cheat"
-    bin.install "bin/snip.neovim.complete.sh" => "snip"
+    bin.install 'bin/snip.neovim.complete.sh' => 'snip'
   end
 
   test do
-    system "false"
+    system 'false'
   end
 end
