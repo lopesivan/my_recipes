a376a80cfb04ee62c12876099d5d066a71df308c
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-08-06 01:02:14 -0300

    Novo método de compilação do neovim

--- neovim@9e1474409.rb
+++ neovim@9e1474409.rb
@@ -12,31 +12,7 @@ class NeovimAT9e1474409< Formula
   head "https://github.com/neovim/neovim.git", branch: "master"
 
   def install
-    args_third_party = %W[
-      --no-warn-unused-cli
-      -DCMAKE_C_COMPILER:FILEPATH=/usr/local/bin/clang
-      -DCMAKE_CXX_COMPILER:FILEPATH=/usr/local/bin/clang++
-    ]
-
-    args = %W[
-      --no-warn-unused-cli
-      -DCMAKE_BUILD_TYPE=Release
-      -DCMAKE_C_COMPILER:FILEPATH=/usr/local/bin/clang
-      -DCMAKE_CXX_COMPILER:FILEPATH=/usr/local/bin/clang++
-      -DCMAKE_INSTALL_PREFIX=#{prefix}
-    ]
-
-    mkdir ".deps" do
-      system "/usr/local/bin/cmake", *args_third_party, "../third-party"
-      system "make"
-    end
-
-    mkdir "build" do
-      system "/usr/local/bin/cmake", *args, ".."
-      system "make"
-      system "make", "install"
-    end
-
+    system "make", "CMAKE_BUILD_TYPE=RelWithDebInfo", "CMAKE_INSTALL_PREFIX=#{prefix}"
   end
 
   test do
