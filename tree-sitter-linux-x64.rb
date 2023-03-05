5b31efc93b4c2a05cc4e42880e6b28738ead4e5a
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-03-05 15:24:00 -0300

    Atualiza o arquivo tree-sitter-linux-x64.rb

--- tree-sitter-linux-x64.rb
+++ tree-sitter-linux-x64.rb
@@ -4,7 +4,7 @@
 class TreeSitterLinuxX64 < Formula
   desc "An incremental parsing system for programming tools"
   homepage "https://github.com/tree-sitter/tree-sitter"
-  url "https://github.com/tree-sitter/tree-sitter/releases/download/v0.20.2/tree-sitter-linux-x64.gz"
+  url "https://github.com/tree-sitter/tree-sitter/releases/download/v0.20.7/tree-sitter-linux-x64.gz"
   sha256 "74ed14a70cc97fc9a357265df442086bd28ef3afea202e1e95874f6d47a59ae0"
   license "BSD-3-Clause"
   revision 4
