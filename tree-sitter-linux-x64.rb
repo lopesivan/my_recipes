5dd247292b329ab1f76347f56070c618f37b7233
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-03-05 15:32:50 -0300

    tree-sitter v0.20.7

--- tree-sitter-linux-x64.rb
+++ tree-sitter-linux-x64.rb
@@ -5,7 +5,7 @@ class TreeSitterLinuxX64 < Formula
   desc "An incremental parsing system for programming tools"
   homepage "https://github.com/tree-sitter/tree-sitter"
   url "https://github.com/tree-sitter/tree-sitter/releases/download/v0.20.7/tree-sitter-linux-x64.gz"
-  sha256 "74ed14a70cc97fc9a357265df442086bd28ef3afea202e1e95874f6d47a59ae0"
+  sha256 "633f47f2239ec45d320258da881a0d2bb2e4383e6b48a0d00713ed402f22d9b1"
   license "BSD-3-Clause"
   revision 4
 
