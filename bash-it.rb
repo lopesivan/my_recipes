05dd03c86fdf098d2b27fed0f1932d4bb48e0dd1
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-08-09 06:06:50 -0300

    [3.8.4] update bash-it.rb

--- bash-it.rb
+++ bash-it.rb
@@ -7,14 +7,14 @@ class BashIt < Formula
   homepage "https://github.com/Bash-it/bash-it"
   url "https://github.com/Bash-it/bash-it.git",
     revision: "2ef5d483e38d790cb1c32ea8999dfbc244ff2f91"
-  version "3.8.3"
-  head "https://github.com/Bash-it/bash-it",
-    branch: "master",
-    using: :git
+  version "3.8.4"
+#  head "https://github.com/Bash-it/bash-it",
+#    branch: "master",
+#    using: :git
   license "GPL-3.0-or-later"
   patch do
-    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/bash-it/bash-it-custom-20230809-4a9fde9c.diff'
-    sha256 '87c36fb93e91bd5f8bb6d17c4fd9d7a2e9eacb88a05d3b382623f28630d0a55c'
+    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/bash-it/bash-it-custom-20230809-d2c26518.diff'
+    sha256 '0c1d190dd5b322a41592d1cd16615bab8f19b4fa85d39dbb6dd001668da8b48c'
   end
   def install
     prefix.install Dir["*"]
