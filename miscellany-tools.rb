f3c5423ff4c07378af18f193383e224271fcf6b3
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-04-01 04:28:01 -0300

    fix(miscellany-tools.rb)
    disable: lsp, lsperm

--- miscellany-tools.rb
+++ miscellany-tools.rb
@@ -23,22 +23,22 @@ class MiscellanyTools < Formula
     # end
 
     bin.install 'bin/bakefile-0.2.12.sh' => 'bakefile.sh'
-    #bin.install "binary/glow" => "glow"
+    # bin.install "binary/glow" => "glow"
     bin.install 'binary/exa' => 'exa'
-    bin.install 'bin/lsp.sh' => 'lsp'
+    # bin.install 'bin/lsp.sh' => 'lsp'
     bin.install 'bin/trans' => 'trans'
-    bin.install 'bin/lsperm.sh' => 'lsperm'
+    # bin.install 'bin/lsperm.sh' => 'lsperm'
     bin.install 'bin/skeleton.sh' => 'skeleton'
     bin.install 'bin/prj.go.sh' => 'prj.go'
     bin.install 'bin/prj.kotlin.sh' => 'prj.kotlin'
     bin.install 'bin/fennel-1.1.0' => 'fennel'
     # bin.install "bin/tree-sitter-0.20.2" => "tree-sitter"
-    bin.install 'bin/java-lsp.sh' => 'java-lsp.sh'
+    # bin.install 'bin/java-lsp.sh' => 'java-lsp.sh'
     bin.install 'bin/winetricks' => 'winetricks'
     bin.install 'bin/cpp-ctags.sh' => 'cpp-ctags'
     bin.install 'bin/cheat-client.sh' => 'cht.sh'
     # bin.install "bin/cheat-client.sh" => "cheat"
-    #bin.install 'bin/snip.neovim.complete.sh' => 'snip'
+    # bin.install 'bin/snip.neovim.complete.sh' => 'snip'
   end
 
   test do
