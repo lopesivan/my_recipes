e9987718dda1ae89bbcc27a9ef751e856afcf702
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-04-01 12:55:38 -0300

    [1.0.2] update nativium@42.rb

--- nativium@42.rb
+++ nativium@42.rb
@@ -3,23 +3,22 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class NativiumAT42 < Formula
-  desc "Write a single code in C++ and compile for any platform."
+  desc "C++ Multiplatform Modular Toolkit Template."
   homepage "https://github.com/nativium/nativium"
   url "https://github.com/nativium/nativium.git",
     revision: "0316d93ad2c21c8c66bdb4b45bc82ba9adcb9e1d"
-  version "1.1.2"
+  version "1.0.2"
 #  head "https://github.com/nativium/nativium",
 #    branch: "main",
 #    using: :git
   license "GPL-3.0-or-later"
   patch do
-    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/nativium/nativium-custom-20240211-cc32bcd.diff'
-    sha256 '11c155774ad9aa73832ae8d75dffa5892d10babab8d82888643bb0353f766564'
+    url 'https://raw.githubusercontent.com/lopesivan/my_patches/main/nativium/nativium-custom-94ca7ead3c8dc56fc9cb3345701efe52ccf4fd8b-20240401.diff'
+    sha256 '9d3b84a940e3228579d9b25aeb217ebb4901f083584a63f3a7435603e71eb806'
   end
   def install
-    prefix.install Dir["*"]
+    prefix.install Dir['*']
   end
-
   test do
     system "false"
   end
